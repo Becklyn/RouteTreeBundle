ad716b728e5adb0f7ede5a3751dc721028a3eee9
Jannik Zschiesche <jz@becklyn.com>
2016-06-28 14:09:05 +0200

    Implemented caching for the tree

--- Cache/TreeCache.php
+++ Cache/TreeCache.php
@@ -2,16 +2,48 @@
 
 namespace Becklyn\RouteTreeBundle\Cache;
 
+use Becklyn\RouteTreeBundle\Tree\Node;
+use Symfony\Component\HttpKernel\HttpKernel;
+use Symfony\Component\HttpKernel\KernelInterface;
+
+
 /**
  *
  */
 class TreeCache
 {
     /**
-     * @return null
+     * @var bool
+     */
+    private $isDebug;
+
+    /**
+     * @var string
+     */
+    private $filePath;
+
+
+    /**
+     * @param KernelInterface $kernel
+     */
+    public function __construct (KernelInterface $kernel)
+    {
+        $this->isDebug = $kernel->isDebug();
+        $this->filePath = $kernel->getCacheDir() . "/becklyn/route-tree/tree.cache";
+    }
+
+
+
+    /**
+     * @return null|Node[]
      */
     public function getTree ()
     {
+        if (!$this->isDebug && is_file($this->filePath))
+        {
+            return unserialize(file_get_contents($this->filePath));
+        }
+
         return null;
     }
 
@@ -22,6 +54,8 @@ class TreeCache
      */
     public function setTree (array $nodes)
     {
-        // to be implemented
+        // checking could be a race condition, so we silently fail if the dir already exists
+        @mkdir(dirname($this->filePath), 0755, true);
+        file_put_contents($this->filePath, serialize($nodes));
     }
 }
