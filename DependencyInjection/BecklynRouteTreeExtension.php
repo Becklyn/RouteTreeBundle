79fabe935c2ff609fe7a71e3c83c0fd23450c984
Jannik Zschiesche <jz@becklyn.com>
2017-12-18 20:12:42 +0100

    Fix extension of yaml file

--- DependencyInjection/BecklynRouteTreeExtension.php
+++ DependencyInjection/BecklynRouteTreeExtension.php
@@ -22,6 +22,6 @@ class BecklynRouteTreeExtension extends Extension
     public function load(array $configs, ContainerBuilder $container)
     {
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('services.yml');
+        $loader->load('services.yaml');
     }
 }
