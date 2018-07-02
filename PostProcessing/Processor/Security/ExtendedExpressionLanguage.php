378bb6ec9fec312f727f84b9342dbb963a606c15
Jannik Zschiesche <jz@becklyn.com>
2018-07-03 00:21:46 +0200

    Add own implementation of security checker

---
+++ PostProcessing/Processor/Security/ExtendedExpressionLanguage.php
@@ -0,0 +1,25 @@
+<?php declare(strict_types=1);
+
+namespace Becklyn\RouteTreeBundle\PostProcessing\Processor\Security;
+
+use Symfony\Component\Security\Core\Authorization\ExpressionLanguage;
+
+
+class ExtendedExpressionLanguage extends ExpressionLanguage
+{
+    /**
+     * @inheritdoc
+     *
+     * Add
+     */
+    protected function registerFunctions()
+    {
+        parent::registerFunctions();
+
+        $this->register('is_granted', function ($attributes, $object = 'null') {
+            return sprintf('$auth_checker->isGranted(%s, %s)', $attributes, $object);
+        }, function (array $variables, $attributes, $object = null) {
+            return $variables['auth_checker']->isGranted($attributes, $object);
+        });
+    }
+}
--- PostProcessing/Processor/Security/SecurityChecker.php
+++ PostProcessing/Processor/Security/SecurityChecker.php
@@ -2,38 +2,80 @@
 
 namespace Becklyn\RouteTreeBundle\PostProcessing\Processor\Security;
 
-use Sensio\Bundle\FrameworkExtraBundle\EventListener\SecurityListener;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\RequestStack;
-use Symfony\Component\HttpKernel\Event\FilterControllerArgumentsEvent;
-use Symfony\Component\HttpKernel\Exception\HttpException;
-use Symfony\Component\HttpKernel\KernelInterface;
-use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use Symfony\Component\Security\Core\Authentication\AuthenticationTrustResolverInterface;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
+use Symfony\Component\Security\Core\Role\Role;
+use Symfony\Component\Security\Core\Role\RoleHierarchyInterface;
 
 
+/**
+ * This implementation should be kept in-sync
+ * with {@see Sensio\Bundle\FrameworkExtraBundle\EventListener\SecurityListener}
+ */
 class SecurityChecker
 {
     /**
-     * @var SecurityListener
+     * @var ExtendedExpressionLanguage
      */
-    private $securityListener;
+    private $language;
+
+    /**
+     * @var TokenStorageInterface
+     */
+    private $tokenStorage;
 
 
     /**
-     * @var KernelInterface
+     * @var RoleHierarchyInterface
      */
-    private $kernel;
+    private $roleHierarchy;
+
+
+    /**
+     * @var AuthenticationTrustResolverInterface
+     */
+    private $trustResolver;
+
+
+    /**
+     * @var AuthorizationCheckerInterface
+     */
+    private $authChecker;
+
+
+    /**
+     * @var RequestStack
+     */
+    private $requestStack;
 
 
     /**
      *
-     * @param SecurityListener $securityListener
-     * @param KernelInterface  $kernel
+     * @param ExtendedExpressionLanguage           $language
+     * @param TokenStorageInterface                $tokenStorage
+     * @param RoleHierarchyInterface               $roleHierarchy
+     * @param AuthenticationTrustResolverInterface $trustResolver
+     * @param AuthorizationCheckerInterface        $authChecker
+     * @param RequestStack                         $requestStack
      */
-    public function __construct (SecurityListener $securityListener, KernelInterface $kernel)
+    public function __construct (
+        ExtendedExpressionLanguage $language,
+        TokenStorageInterface $tokenStorage,
+        RoleHierarchyInterface $roleHierarchy,
+        AuthenticationTrustResolverInterface $trustResolver,
+        AuthorizationCheckerInterface $authChecker,
+        RequestStack $requestStack
+    )
     {
-        $this->securityListener = $securityListener;
-        $this->kernel = $kernel;
+        $this->language = $language;
+        $this->tokenStorage = $tokenStorage;
+        $this->roleHierarchy = $roleHierarchy;
+        $this->trustResolver = $trustResolver;
+        $this->authChecker = $authChecker;
+        $this->requestStack = $requestStack;
     }
 
 
@@ -45,32 +87,43 @@ class SecurityChecker
      */
     public function canAccess (string $expression) : bool
     {
-        try
+        $request = $this->requestStack->getMasterRequest();
+
+        if (null === $request)
         {
-            // create a fake request and a fake event, as we only need to fill the values,
-            // the SecurityListener from the FrameworkExtraBundle actually uses.
-            $request = new Request([], [], [
-                "_security" => [$expression],
-            ]);
-
-            // create a fake event -> we only care about the request arguments
-            $fakeEvent = new FilterControllerArgumentsEvent(
-                $this->kernel,
-                function () {},
-                [],
-                $request,
-                null
-            );
-
-            $this->securityListener->onKernelControllerArguments($fakeEvent);
-
-            // no exception was thrown, so allow access
-            return true;
-        }
-        catch (HttpException | AccessDeniedException $exception)
-        {
-            // an exception was thrown, so access was denied
+            // we have a security expression but no request -> be safe and deny access
             return false;
         }
+
+        return (bool) $this->language->evaluate($expression, $this->getVariables($request));
+    }
+
+
+    /**
+     * Returns the required variables for the expression language
+     *
+     * @param Request $request
+     * @return array
+     */
+    private function getVariables (Request $request) : array
+    {
+        $token = $this->tokenStorage->getToken();
+
+        $roles = (null !== $this->roleHierarchy)
+            ? $this->roleHierarchy->getReachableRoles($token->getRoles())
+            : $token->getRoles();
+
+        // we only need the main variables, as we have no controller here
+        return [
+            'token' => $token,
+            'user' => $token->getUser(),
+            'object' => $request,
+            'subject' => $request,
+            'request' => $request,
+            'roles' => array_map(function (Role $role) { return $role->getRole(); }, $roles),
+            'trust_resolver' => $this->trustResolver,
+            // needed for the is_granted expression function
+            'auth_checker' => $this->authChecker,
+        ];
     }
 }
