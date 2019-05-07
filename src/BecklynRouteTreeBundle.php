7daa93043c86846513dae77bab3e9454b868cc8d
Jannik Zschiesche <jz@becklyn.com>
2019-05-07 11:39:13 +0200

    Simplify bundle class

--- src/BecklynRouteTreeBundle.php
+++ src/BecklynRouteTreeBundle.php
@@ -4,7 +4,10 @@ declare(strict_types=1);
 
 namespace Becklyn\RouteTreeBundle;
 
-use Becklyn\RouteTreeBundle\DependencyInjection\BecklynRouteTreeExtension;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 
@@ -16,8 +19,18 @@ class BecklynRouteTreeBundle extends Bundle
     /**
      * @inheritDoc
      */
-    protected function getContainerExtensionClass ()
+    public function getContainerExtension ()
     {
-        return BecklynRouteTreeExtension::class;
+        return new class() extends Extension
+        {
+            /**
+             * @inheritDoc
+             */
+            public function load(array $configs, ContainerBuilder $container)
+            {
+                $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
+                $loader->load('services.yaml');
+            }
+        };
     }
 }
